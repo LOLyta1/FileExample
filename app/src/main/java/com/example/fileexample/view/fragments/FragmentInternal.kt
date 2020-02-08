7bebf0e9b2636d9f68d1784a3f94d74b55c6bf69
LOLyta1 <nata_tsivileva@mail.ru>
2020-02-09 04:12:14 +0600

    FILEX-11 #comment сделан диалог выбора хранилища перед сохранением

--- app/src/main/java/com/example/fileexample/view/fragments/FragmentInternal.kt
+++ app/src/main/java/com/example/fileexample/view/fragments/FragmentInternal.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -16,17 +14,18 @@ import com.example.fileexample.R
 import com.example.fileexample.StorageTypes
 import com.example.fileexample.presenter.FilePresenter
 import com.example.fileexample.view.IView
-import com.example.fileexample.view.recycler.LogAdapter
+import com.example.fileexample.view.dialogs.DialogStorage
+import com.example.fileexample.view.recyclers.LogAdapter
 import kotlinx.android.synthetic.main.fragment_text_file.*
 import kotlinx.android.synthetic.main.fragment_text_file.view.*
 import java.lang.Exception
 
 class FragmentInternal() :
     Fragment(),
-    IView {
+    IView,
+    DialogStorage.Listener {
 
     private var presenter: FilePresenter? = null
-    private var storageType: StorageTypes = StorageTypes.INTERNAL
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,87 +36,78 @@ class FragmentInternal() :
         return inflater.inflate(R.layout.fragment_text_file, container, false)
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        setupRecycler()
-        setupSpinner()
+        val dialog = DialogStorage().apply {setTargetFragment(this@FragmentInternal, 10)}
+        val manager = activity?.supportFragmentManager
 
         view.save_button.setOnClickListener {
-            val fileName = file_name_edit_text.text.toString()
-            val fileText = file_text_edit_text.text.toString()
-            presenter?.createFile(fileName, storageType)
-            presenter?.saveToFile(fileName, fileText)
+            if (manager != null) {
+                dialog.arguments = Bundle().apply { putInt("mode", DialogStorage.SAVE_MODE)}
+                dialog.show(manager, "dialog")
+            }
         }
 
         view.load_button.setOnClickListener {
-            presenter?.readFile(file_name_edit_text.text.toString())
+            if (manager != null) {
+                dialog.arguments = Bundle().apply { putInt("mode", DialogStorage.LOAD_MODE)}
+                dialog.show(manager, "dialog")
+            }
         }
-
-        logs_recycler_view.apply {
-            layoutManager = LinearLayoutManager(mContext)
-            adapter = LogAdapter()
-        }
-
+        setupRecycler()
     }
+
+    private fun setupRecycler() {
+        if (view != null) {
+            view!!.logs_recycler_view.apply {
+                layoutManager = LinearLayoutManager(mContext)
+                adapter = LogAdapter()
+            }
+            view!!.logs_recycler_view?.addItemDecoration(
+                DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL).apply {
+                    setDrawable(resources.getDrawable(R.drawable.my_devider))
+                }
+            )
+        }
+    }
+
     override val mContext: Context?
         get() = context
 
     override fun showFileText(fileText: String) {
-        AlertDialog.Builder(mContext).setIcon(android.R.drawable.ic_dialog_info)
+        logIt(fileText)
+        AlertDialog.Builder(mContext)
+            .setIcon(android.R.drawable.ic_dialog_info)
             .setTitle("Текст файла:").setMessage(fileText)
-            .setPositiveButton("OK") { dialog, which -> dialog.dismiss() }.show()
-        ((logs_recycler_view.adapter) as LogAdapter).addItem("Текст файла: $fileText")
+            .setPositiveButton("OK") { dialog, which -> dialog.dismiss() }
+            .show()
+
     }
 
     override fun showError(ex: Exception) {
         val errorText = "Ошибка чтения файла - ${ex}"
+        logIt(errorText)
         Toast.makeText(context, errorText, Toast.LENGTH_LONG).show()
-        ((logs_recycler_view.adapter) as LogAdapter).addItem(errorText)
     }
 
     override fun showLog(message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-        ((logs_recycler_view.adapter) as LogAdapter).addItem(message)
+        logIt(message)
     }
 
-    private fun setupRecycler() {
-        val decorator = DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL)
-        decorator.setDrawable(resources.getDrawable(R.drawable.my_devider))
-        view?.logs_recycler_view?.addItemDecoration(decorator)
+    private fun logIt(text: String) {
+        ((view?.logs_recycler_view?.adapter) as LogAdapter).addItem(text)
     }
 
-    private fun setupSpinner() {
-        if (mContext != null) {
-
-            view?.spinner?.adapter = ArrayAdapter.createFromResource(
-                mContext!!,
-                R.array.spinner_item,
-                android.R.layout.simple_spinner_item
-            )
-
-            view?.spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                }
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
-                    storageType = when (position) {
-                        0 -> StorageTypes.INTERNAL
-                        1 -> StorageTypes.EXTERNAL_MEDIA
-                        2 -> StorageTypes.EXTERNAL_CACHE
-                        3 -> StorageTypes.INTERNAL_CACHE
-                        else -> StorageTypes.INTERNAL
-                    }
-                }
+    override fun dialogOkCLick(storage: StorageTypes, mode: Int?) {
+        val fileName = file_name_edit_text.text.toString()
+        val fileText = file_text_edit_text.text.toString()
+        when(mode){
+           DialogStorage.SAVE_MODE->{
+               presenter?.createFile(fileName, fileText,storage)
+           }
+            DialogStorage.LOAD_MODE->{
+                presenter?.readFile(fileName,storage)
             }
         }
     }
-
-
-
 }
