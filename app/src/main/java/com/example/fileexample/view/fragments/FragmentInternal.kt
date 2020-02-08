fe0d73b059ff5158c030edcc2a6a779607bcfeb8
LOLyta1 <33492646+LOLyta1@users.noreply.github.com>
2020-02-08 21:59:30 +0600

    Revert "FILEX-1 вместо radio button сделан spinner"

--- app/src/main/java/com/example/fileexample/view/fragments/FragmentInternal.kt
+++ app/src/main/java/com/example/fileexample/view/fragments/FragmentInternal.kt
@@ -6,12 +6,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fileexample.R
 import com.example.fileexample.StorageTypes
 import com.example.fileexample.presenter.FilePresenter
@@ -26,7 +25,6 @@ class FragmentInternal() :
     IView {
 
     private var presenter: FilePresenter? = null
-    private var storageType: StorageTypes = StorageTypes.INTERNAL
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,37 +40,55 @@ class FragmentInternal() :
         super.onViewCreated(view, savedInstanceState)
 
         setupRecycler()
-        setupSpinner()
 
         view.save_button.setOnClickListener {
             val fileName = file_name_edit_text.text.toString()
             val fileText = file_text_edit_text.text.toString()
-            presenter?.createFile(fileName, storageType)
-            presenter?.saveToFile(fileName, fileText)
+            lateinit var storageType:StorageTypes
+
+            when(radio_group.checkedRadioButtonId){
+                R.id.internal_text_file_radio_button -> storageType=StorageTypes.INTERNAL
+                R.id.external_text_file_radio_button -> storageType=StorageTypes.EXTERNAL_MEDIA
+                R.id.internal_cahe_text_file_radio_button ->storageType=StorageTypes.INTERNAL_CACHE
+                R.id.external_cache_text_file_radio_button ->storageType=StorageTypes.EXTERNAL_CACHE
+            }
+                presenter?.createFile(fileName,storageType)
+                presenter?.saveToFile(fileName, fileText)
         }
+
 
         view.load_button.setOnClickListener {
             presenter?.readFile(file_name_edit_text.text.toString())
         }
 
         logs_recycler_view.apply {
-            layoutManager = LinearLayoutManager(mContext)
-            adapter = LogAdapter()
+            layoutManager=LinearLayoutManager(mContext)
+            adapter=LogAdapter()
         }
 
     }
+
+    private fun setupRecycler() {
+        val decorator=DividerItemDecoration(context,DividerItemDecoration.HORIZONTAL)
+        decorator.setDrawable(resources.getDrawable(R.drawable.my_devider))
+        view?.logs_recycler_view?.addItemDecoration(decorator)
+    }
+
     override val mContext: Context?
         get() = context
 
     override fun showFileText(fileText: String) {
-        AlertDialog.Builder(mContext).setIcon(android.R.drawable.ic_dialog_info)
-            .setTitle("Текст файла:").setMessage(fileText)
-            .setPositiveButton("OK") { dialog, which -> dialog.dismiss() }.show()
+      AlertDialog.Builder(mContext).
+          setIcon(android.R.drawable.ic_dialog_info).
+          setTitle("Текст файла:").
+          setMessage(fileText).
+          setPositiveButton("OK") { dialog, which ->dialog.dismiss()}.
+          show()
         ((logs_recycler_view.adapter) as LogAdapter).addItem("Текст файла: $fileText")
     }
 
     override fun showError(ex: Exception) {
-        val errorText = "Ошибка чтения файла - ${ex}"
+        val errorText="Ошибка чтения файла - ${ex}"
         Toast.makeText(context, errorText, Toast.LENGTH_LONG).show()
         ((logs_recycler_view.adapter) as LogAdapter).addItem(errorText)
     }
@@ -81,43 +97,6 @@ class FragmentInternal() :
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
         ((logs_recycler_view.adapter) as LogAdapter).addItem(message)
     }
-
-    private fun setupRecycler() {
-        val decorator = DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL)
-        decorator.setDrawable(resources.getDrawable(R.drawable.my_devider))
-        view?.logs_recycler_view?.addItemDecoration(decorator)
-    }
-
-    private fun setupSpinner() {
-        if (mContext != null) {
-
-            view?.spinner?.adapter = ArrayAdapter.createFromResource(
-                mContext!!,
-                R.array.spinner_item,
-                android.R.layout.simple_spinner_item
-            )
-
-            view?.spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                }
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
-                    storageType = when (position) {
-                        0 -> StorageTypes.INTERNAL
-                        1 -> StorageTypes.EXTERNAL_MEDIA
-                        2 -> StorageTypes.EXTERNAL_CACHE
-                        3 -> StorageTypes.INTERNAL_CACHE
-                        else -> StorageTypes.INTERNAL
-                    }
-                }
-            }
-        }
-    }
-
 
 
 }
